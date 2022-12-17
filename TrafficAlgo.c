5bcc8df851f2b8a1dbf9774149d2bdde54fe681c
Ganesh Dagadi <73548263+ganesh-dagadi@users.noreply.github.com>
2022-12-17 18:46:00 +0530

    Completed Algo. Error

--- TrafficAlgo.c
+++ TrafficAlgo.c
@@ -5,7 +5,10 @@
 #define MAX_JUNCS 10
 #define MAX_ROADS 10
 #define NUM_JUNCS 5
-#define NUM_VEHICLES 7
+#define NUM_VEHICLES 5
+
+int sourceNode = 2;
+int destNode = 0;
 
 typedef struct road {
     int to;
@@ -28,18 +31,19 @@ node nodes[MAX_JUNCS];
 void initializeNodes() {
     int i = 0;
     for (i; i < MAX_JUNCS; i++) {
-        nodes[i].id = 0;
+        nodes[i].id = -1;
     }
 }
 void createNode(int x, int y) {
     int i;
     int j;
-    for (i = 1; i < MAX_JUNCS; i++) {
-        if (nodes[i].id == 0) {
+    for (i = 0; i < MAX_JUNCS; i++) {
+        if (nodes[i].id == -1) {
             nodes[i].id = i;
             nodes[i].x = x;
             nodes[i].y = y;
             nodes[i].explored = 0;
+            printf("Node is %d , x is %d , y is %d \n" , nodes[i].id , nodes[i].x , nodes[i].y);
             for (j = 0; j < MAX_ROADS; j++) {
                 nodes[i].roads[j] = NULL;
             }
@@ -49,7 +53,7 @@ void createNode(int x, int y) {
 }
 
 void printNodes() {
-    int i = 1;
+    int i = 0;
     int j = 0;
     for (i; i < MAX_JUNCS; i++) {
         printf("Node is %d x %d y %d \n" , nodes[i].id , nodes[i].x , nodes[i].y);
@@ -105,7 +109,7 @@ void addRoads(int from , int to , int capacity) {
         }
     }
 }
-
+//TODO VERIFY The next 2 FUNC WORK
 void removeRoad(road* rd, int fromNodeId) {
     node* toNode = getNode(rd->to);
     printf("%d \n ", toNode->id);
@@ -120,6 +124,10 @@ void removeRoad(road* rd, int fromNodeId) {
         }
     }
 }
+
+void changeRoadCapacity(road* rd , int capacity) {
+    rd->capacity = capacity;
+}
 void removeNode(int nodeId) {
     node* toRemove = getNode(nodeId);
     //set node id to 0 (it is not valid)
@@ -130,6 +138,143 @@ void removeNode(int nodeId) {
             toRemove->roads[j] = NULL;
         }
     }
-    toRemove->id = 0;
+    toRemove->id = -1;
+}
+
+node* getSetupData() {
+   return nodes;
+}
+
+int generatePaths(int arr[NUM_VEHICLES][MAX_ROADS]) {
+    int remainingVehicles = NUM_VEHICLES;
+    int currentNode = sourceNode;
+    //initial shortest distance being set to infinity
+    for (int i = 0; i < NUM_JUNCS; i++) {
+        nodes[i].shortestDis = INT_MAX;
+    }
+
+    //starting main loop
+    // exit when either all vehicles have passed or road has been blocked.
+   
+    while (remainingVehicles) {
+        short int laneFound = 0;
+        nodes[sourceNode].explored = 1;
+        nodes[sourceNode].prev = nodes[sourceNode].id;
+        nodes[sourceNode].shortestDis = 0;
+        nodes[sourceNode].currDis = 0;
+
+        while (currentNode != destNode) {
+            laneFound = 0;
+            for (int i = 0; i < MAX_ROADS; i++) {
+                if (nodes[currentNode].roads[i] == NULL) break;
+                road currentRoad = *(nodes[currentNode].roads[i]);
+                if (!nodes[currentRoad.to].explored && (currentRoad.capacity > 0)) {
+                    nodes[currentRoad.to].currDis = nodes[currentNode].shortestDis + currentRoad.dis;
+                    laneFound = 1;
+                    if (nodes[currentRoad.to].currDis < nodes[currentRoad.to].shortestDis) {
+                        nodes[currentRoad.to].shortestDis = nodes[currentRoad.to].currDis;
+                        nodes[currentRoad.to].prev = nodes[currentNode].id;
+                    }
+                }
+            }
+            if (!laneFound) {
+                goto laneNotFound;
+            };
+            int nearestNode;
+            for (int i = 0; i < MAX_ROADS; i++) {
+                if (nodes[currentNode].roads[i] == NULL) break;
+                road currentRoad = *(nodes[currentNode].roads[i]);
+                if (!nodes[currentRoad.to].explored) nearestNode = nodes[currentRoad.to].id;
+                else continue;
+            }
+
+            for (int i = 0; i < MAX_ROADS; i++) {
+                if (nodes[currentNode].roads[i] == NULL) break;
+                road currentRoad = *(nodes[currentNode].roads[i]);
+                if (!nodes[currentRoad.to].explored && (nodes[currentRoad.to].shortestDis < nodes[nearestNode].shortestDis)) {
+                    nearestNode = nodes[currentRoad.to].id;
+                }
+            }
+            nodes[nearestNode].explored = 1;
+            currentNode = nearestNode;
+        }
+
+        //traverse back to start
+        int juncNum = 0;
+        while (1) {
+
+            for (int i = 0; i < MAX_ROADS; i++) {
+                if (nodes[currentNode].roads[i]->to == nodes[currentNode].prev) {
+                    nodes[currentNode].roads[i]->capacity -= 1;
+                    break;
+                }
+            }
+            int prev = nodes[currentNode].prev;
+            for (int i = 0; i < MAX_ROADS; i++) {
+                if (nodes[prev].roads[i]->to == currentNode) {
+                    nodes[prev].roads[i]->capacity -= 1;
+                    break;
+                }
+            }
+
+            //registering the path
+            arr[NUM_VEHICLES - remainingVehicles][juncNum] = currentNode;
+            currentNode = nodes[currentNode].prev;
+            ++juncNum;
+            if (currentNode == sourceNode) {
+                arr[NUM_VEHICLES - remainingVehicles][juncNum] = currentNode;
+                break;
+            };
+        }
+        remainingVehicles -= 1;
+        currentNode = sourceNode;
+        for (int i = 0; i < MAX_JUNCS; i++) {
+            nodes[i].explored = 0;
+            nodes[i].currDis = INT_MAX;
+            nodes[i].shortestDis = INT_MAX;
+        }
+
+    }
+
+    return 1;
+    laneNotFound:
+        printf("");
+        int juncNum = 0;
+        while (1) {
+            arr[NUM_VEHICLES - remainingVehicles][juncNum] = currentNode;
+            currentNode = nodes[currentNode].prev;
+            ++juncNum;
+            if (currentNode == sourceNode) {
+                arr[NUM_VEHICLES - remainingVehicles][juncNum] = currentNode;
+                break;
+            }
+        }
+        return 0;
+}
+
+void printRouteOutput(int arr[NUM_VEHICLES][MAX_ROADS]) {
+    for (int i = 0; i < NUM_VEHICLES; i++) {
+        for (int j = 0; j < MAX_ROADS; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int** generateRoutesOutput(int arr[NUM_VEHICLES][MAX_ROADS]) {
+    
+    for (int i = 0; i < NUM_VEHICLES; i++) {
+        for (int j = 0; j < MAX_ROADS; j++) {
+            arr[i][j] = -1;
+        }
+    }
+    if (generatePaths(arr)) {
+        printf("\nLane Found \n");
+    }
+    else {
+        printf("\n Lane not found \n");
+    }
+    printRouteOutput(arr);
+    return arr;
 }
 
--- service.c
+++ service.c
@@ -5,16 +5,45 @@ int main() {
 	createNode(5 , 8);
 	createNode(7, 6);
 	createNode(8, 2);
-	createNode(2, 2);
-	addRoads(1, 2 , 2);
-	addRoads(2, 3, 2);
-	printNodes();
-	printf("Removing node \n");
-	removeNode(2);
-	printf("printing node \n");
-	printNodes();
-	createNode(7, 6);
-	printf("Printing Nodes \n");
-	printNodes();
+	createNode(5, 4);
+	//createNode(2, 2);
+	createNode(2, 6);
+	//createNode(5, 4);
+	addRoads(0, 1 , 2);
+	addRoads(1, 2, 1);
+	addRoads(1, 3, 1);
+	addRoads(2, 3, 3);
+	addRoads(0, 4, 3);
+	addRoads(4, 3, 3);
+	//addRoads(6, 2, 1);
+	//addRoads(6, 5, 2);
+	//addRoads(5, 4, 4);
+	//addRoads(4, 3, 4);
+	//addRoads(3, 2, 3);
+	node* nodes = getSetupData();
+	printf("Here");
+	for (int i = 0; i < MAX_JUNCS; i++) {
+		if (nodes[i].id == -1) {
+			continue;
+		}
+		printf("Node id is %d coordinates(%d , %d) \n", nodes[i].id, nodes[i].x, nodes[i].y);
+		for (int j = 0; j < MAX_ROADS; j++) {
+			if (nodes[i].roads[j] == NULL) {
+				continue;
+			}
+			printf("Road is from %d to %d with capacity %d and distance %f \n", nodes[i].id, nodes[i].roads[j]->to, nodes[i].roads[j]->capacity, nodes[i].roads[j]->dis);
+		}
+		printf("New junction \n");
+	}
+
+	int arr[NUM_VEHICLES][MAX_ROADS];
+	generateRoutesOutput(arr);
+	printf("Here");
+	for (int i = 0; i < NUM_VEHICLES; i++) {
+		for (int j = 0; j < MAX_ROADS; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
